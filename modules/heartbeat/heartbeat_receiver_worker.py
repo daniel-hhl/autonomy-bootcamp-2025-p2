9339a9e1d28d45327b0c70a2ddb4d8f2384dbdec
Daniel <danielcockingson@gmail.com>
2025-12-18 14:57:25 -0500

    Recovered Deleted Receiver Worker

---
+++ modules/heartbeat/heartbeat_receiver_worker.py
@@ -0,0 +1,74 @@
+"""
+Heartbeat worker that sends heartbeats periodically.
+"""
+
+import os
+import pathlib
+
+from pymavlink import mavutil
+
+from utilities.workers import queue_proxy_wrapper
+from utilities.workers import worker_controller
+from . import heartbeat_receiver
+from ..common.modules.logger import logger
+
+
+# =================================================================================================
+#                            ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
+# =================================================================================================
+def heartbeat_receiver_worker(
+    connection: mavutil.mavfile,
+    local_logger: logger.Logger,
+    output_queue: queue_proxy_wrapper.QueueProxyWrapper,
+    controller: worker_controller.WorkerController,
+) -> None:
+    """
+    Worker process.
+
+    connection: connection between drone and worker
+    local_logger: logs info, errors, etc. if they occur
+    output_queue: deals with the queue of outputs to main process
+    controller: controlls worker process
+    """
+    # =============================================================================================
+    #                          ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
+    # =============================================================================================
+
+    # Instantiate logger
+    worker_name = pathlib.Path(__file__).stem
+    process_id = os.getpid()
+    result, local_logger = logger.Logger.create(f"{worker_name}_{process_id}", True)
+    if not result:
+        print("ERROR: Worker failed to create logger")
+        return
+
+    # Get Pylance to stop complaining
+    assert local_logger is not None
+
+    local_logger.info("Logger initialized", True)
+
+    # =============================================================================================
+    #                          ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
+    # =============================================================================================
+    # Instantiate class object (heartbeat_receiver.HeartbeatReceiver)
+    creation_status, receiver = heartbeat_receiver.HeartbeatReceiver.create(
+        connection, local_logger
+    )
+
+    if not creation_status:
+        local_logger.error("Could not initialize Heartbeat Receiver")
+    else:
+        # Main loop: do work.
+        while not controller.is_exit_requested():
+            controller.check_pause()
+
+            value, status = receiver.run()
+            if not value:
+                local_logger.warning("Failed to receive Heartbeat")
+            output_queue.queue.put(status)
+        connection.close()
+
+
+# =================================================================================================
+#                            ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
+# =================================================================================================
